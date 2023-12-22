0dc50d2852fdadcc5c5cd4c881d635774091c5da
TruddyTheDuddi <ddemkoo.nolkooo@gmail.com>
2023-12-22 18:28:06 +0100

    Finished graphgit status

--- js/graphs2.js
+++ js/graphs2.js
@@ -4,22 +4,25 @@ var height = 600;
 
 const DESC = {
     avg_slope_change_significant: {
-        title: "Average slope change",
-        desc: "Measure of the (signi) mean impact of a film genre on baby names"
+        title: "Average influence yileded by genres",
+        desc: "Measuring the mean impact of a film genre on baby names (value in convexity factor).",
+        yLabel: "Average slope change"
     },
     avg_mag_slope_change_significant: {
-        title: "Average slope change magnitude",
-        desc: "Measure of the (signi) mean magnitude impact of a film genre on baby names"
+        title: "Average absolute influence yielded by genres",
+        desc: "Measure of the mean \"magnitude\" impact as absolute values of a film genre on baby names (value in convexity factor).",
+        yLabel: "Average magnitude of slope change"
     },
-    prop_names_signi_in_genre_per_total_film_in_genre: {
-        title: "Names significant per movie genre",
-        desc: "Proportion of significantly impacted names in a given movie genre divided by the number of film in the corrresponding genre"
+    prop_signif_per_genre: {
+        title: "Proportion of influenced names by genres",
+        desc: "Proportion of significantly impacted names in a given movie genre (values in percentage).",
+        yLabel: "Proportion of significantly impacted names"
     }
 }
 
 // Global variable to store the current data key
 var currentDataKeyIndex = 0;
-var dataKeys = ["avg_slope_change_significant", "avg_mag_slope_change_significant", "prop_names_signi_in_genre_per_total_film_in_genre"];
+var dataKeys = ["avg_slope_change_significant", "avg_mag_slope_change_significant", "prop_signif_per_genre"];
 var currentDataKey = dataKeys[currentDataKeyIndex];
 
 // append the svg object to the body of the page
@@ -46,11 +49,6 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
         .append("div")
         .style("opacity", 0)
         .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "2px")
-        .style("border-radius", "5px")
-        .style("padding", "5px");
 
     // Three functions that change the tooltip when the user hovers/moves/leaves a circle –> !!!To display movie genre name + value!!!
     var mouseover1 = function(d) {
@@ -58,10 +56,21 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
     };
     var mousemove1 = function(event, d) {
         var pointer = d3.pointer(event);
-        Tooltip1.html('<u>' + d.genre + '</u>' + "<br>" + d[currentDataKey])
+        Tooltip1.html(`<span class="genre_name">${d.genre}</span><span class="genre_val">${adaptTooltipVal(d[currentDataKey])}</span>`)
             .style("left", (pointer[0] + 20) + "px")
             .style("top", (pointer[1]) + "px");
     };
+
+    function adaptTooltipVal(value){
+        value = +value;
+        if(currentDataKey === "prop_signif_per_genre"){
+            // This is a percentage, display as such
+            return `${(value * 100).toFixed(2)}%`;
+        } else {
+            // This is a number, display as such, at most 5 decimlas
+            return value.toFixed(5);
+        }
+    }
 
     var mouseleave1 = function(d) {
         Tooltip1.style("opacity", 0);
@@ -82,11 +91,8 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
         .attr("r", function(d) {
             return size(Math.abs(+d[currentDataKey]));
         }) // Using Math.abs to get the absolute value
-        // .attr("cx", width / 2)
-        // Set the initial x position of the data based on the sign of the value
-        //.style("fill", function (d) { return "hsl(" + Math.random() * 360 + ",80%,50%)"; }) // Random fill color
         .style("stroke", function(d) {
-            return +d[currentDataKey] >= 0 ? "red" : "blue";
+            return +d[currentDataKey] >= 0 ? colors.p : colors.n;
         }) // Set stroke color based on the sign
         .style("fill-opacity", 1)
         // .attr("stroke", "black")
@@ -94,10 +100,10 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
         .on("mouseover", mouseover1)
         .on("mousemove", mousemove1)
         .on("mouseleave", mouseleave1)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+        // .call(d3.drag()
+        //     .on("start", dragstarted)
+        //     .on("drag", dragged)
+        //     .on("end", dragended));
 
     var textLabels = svg.append("g")
         .selectAll("text")
@@ -137,7 +143,7 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                 return +d.avg_slope_change_significant >= 0 ? 0.8 * width / 4 : 2.6 * width / 4;
             } else if (currentDataKey === "avg_mag_slope_change_significant") {
                 return +d.avg_slope_change_significant >= 0 ? width / 4 : 2.6 * width / 4;
-            } else if (currentDataKey === "prop_names_signi_in_genre_per_total_film_in_genre") {
+            } else if (currentDataKey === "prop_signif_per_genre") {
                 return +d.nb_films_in_genre >= 10000 ? width / 4 : 3 * width / 4;
             }
         }))
@@ -147,7 +153,7 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                 return +d.avg_slope_change_significant >= 0 ? height / 2 : height / 2;
             } else if (currentDataKey === "avg_mag_slope_change_significant") {
                 return +d.avg_slope_change_significant >= 0 ? height / 2 : height / 2;
-            } else if (currentDataKey === "prop_names_signi_in_genre_per_total_film_in_genre") {
+            } else if (currentDataKey === "prop_signif_per_genre") {
                 return +d.nb_films_in_genre >= 10000 ? height / 2 : height / 2;
             }
         }));
@@ -243,7 +249,7 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                 } else if (currentDataKey === "avg_mag_slope_change_significant") {
                     console.log("Setting color for avg_mag_slope_change_significant");
                     return "rgba(0,0,0,0.4)";
-                } else if (currentDataKey === "prop_names_signi_in_genre_per_total_film_in_genre") {
+                } else if (currentDataKey === "prop_signif_per_genre") {
                     // Set fill color based on the number of films in genre
                     return +d.nb_films_in_genre > 10000 ? "rgba(0,0,0,0.4)" : "rgba(0,0,0,0.2)";
                 }
@@ -264,7 +270,7 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
         width2 = 700 - margin.left - margin.right,
         height2 = 350 - margin.top - margin.bottom;
 
-    // append the svg object to the body of the page
+    // INIT: append the svg object to the body of the page
     var svgDistributionPerYear = d3
         .select("#DistributionPerYear")
         .append("svg")
@@ -273,7 +279,28 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Set X axis title
+    var xLabel = svgDistributionPerYear.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "end")
+        .attr("x", width2)
+        .attr("y", height2 + 30)
+        .text("Year")
+        .attr("font-size", "0.8em")
+        .style("opacity", 0.5);
 
+    // Set Y axis title
+    var yLabel = svgDistributionPerYear.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("x", 0)
+        .attr("y", -45)
+        .attr("transform", "rotate(-90)")
+        .attr("font-size", "0.8em")
+        .style("opacity", 0.5)
+        .text("Value");
+
+    // Load data
     d3.csv("data/movie_genre_per_year_significant.csv").then(data2 => {
         var x = d3.scaleLinear().domain([1888, 2016]).range([0, width2]);
         var y = d3.scaleLinear().domain([-0.24, 0.24]).range([height2, 0]);
@@ -293,20 +320,34 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
             .attr("stroke-width", 1.5);
 
         svg.selectAll(".node")
-            .on("mouseover", function(event, d) {
+            .on("click", function(event, d) {
                 var selectedGenre = d.genre;
                 var filteredData = data2.filter(entry => entry.genre === selectedGenre);
                 updateLineChart(filteredData);
                 mouseover1.call(this, event, d);
+
+                // Add overlay to the graph
+                document.getElementById("curser_af_graph").classList.add("overlay");
+
+                // If mouse moved, remove overlay after 0.3 seconds
+                setTimeout(() => {
+                    document.getElementById("curser_af_graph").addEventListener("mousemove", function() {
+                        document.getElementById("curser_af_graph").classList.remove("overlay");
+                    }, { once: true });
+                }, 300);
             })
-            .on("mousemove", mousemove1)
+            // .on("mousemove", mousemove1)
             .on("mouseleave", function() {
-                updateLineChart(data2);
                 mouseleave1.call(this);
             });
 
         function updateLineChart(data2) {
             svgDistributionPerYear.selectAll(".node").remove();
+
+            // Update Y axis title
+            yLabel.text(DESC[currentDataKey].yLabel);
+
+            // Add circles
             svgDistributionPerYear.selectAll(".node")
                 .data(data2)
                 .enter()
@@ -316,7 +357,13 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                     return x(d.year);
                 })
                 .attr("cy", function(d) {
-                    return y(+d.avg_slope_change_significant);
+                    // Plot the y of the current data key
+                    if(currentDataKey != "prop_signif_per_genre"){
+                        return y(+d[currentDataKey]);
+                    } else {
+                        return y(+d["prop_signif_per_genre_per_year"]);
+                    }
+
                 })
                 .attr("r", 1) // Adjust the radius as needed
                 .style("fill", "blue") // Adjust the fill color as needed
@@ -325,19 +372,46 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                 }); // Attach data to circles
 
             // Update area path
-            area.datum(data2).attr("d", d3.area()
-                .x(function(d) {
-                    return x(d.year);
-                })
-                .y0(function(d) {
-                    var lowerBound = (+d.avg_slope_change_significant - 1.96 * d.se_slope_change_significant);
-                    return y(isFinite(lowerBound) ? lowerBound : 0); // Set the bottom of the filled region to the lower bound
-                })
-                .y1(function(d) {
-                    var upperBound = (+d.avg_slope_change_significant + 1.96 * d.se_slope_change_significant);
-                    return y(isFinite(upperBound) ? upperBound : 0); // Set the top of the filled region to the upper bound
-                })
-            );
+            if(currentDataKey != "prop_signif_per_genre"){
+                area.datum(data2).attr("d", d3.area()
+                    .x(function(d) {
+                        return x(d.year);
+                    })
+                    .y0(function(d) {
+                        let se = 0;
+                        if(currentDataKey === "avg_slope_change_significant"){
+                            se = d.se_slope_change_significant;
+                        } else {
+                            se = d.se_slope_change_magnitude_significant;
+                        }
+                        var lowerBound = (+d[currentDataKey] - 1.96 * se);
+                        return y(isFinite(lowerBound) ? lowerBound : 0); // Set the bottom of the filled region to the lower bound
+                    })
+                    .y1(function(d) {
+                        let se = 0;
+                        if(currentDataKey === "avg_slope_change_significant"){
+                            se = d.se_slope_change_significant;
+                        } else {
+                            se = d.se_slope_change_magnitude_significant;
+                        }
+                        var upperBound = (+d[currentDataKey] + 1.96 * se);
+                        return y(isFinite(upperBound) ? upperBound : 0); // Set the top of the filled region to the upper bound
+                    })
+                );
+            } else {
+                // Reset
+                area.datum(data2).attr("d", d3.area()
+                    .x(function(d) {
+                        return x(d.year);
+                    })
+                    .y0(function(d) {
+                        return y(0); // Set the bottom of the filled region to the lower bound
+                    })
+                    .y1(function(d) {
+                        return y(0); // Set the top of the filled region to the upper bound
+                    })
+                );
+            }
 
             // Update line path
             line.datum(data2).attr("d", d3.line()
@@ -345,7 +419,11 @@ d3.csv("data/movie_genre_significant.csv").then(data => {
                     return x(d.year);
                 })
                 .y(function(d) {
-                    return y(+d.avg_slope_change_significant);
+                    if(currentDataKey != "prop_signif_per_genre"){
+                        return y(+d[currentDataKey]);
+                    } else {
+                        return y(+d["prop_signif_per_genre_per_year"]);
+                    }
                 })
             );
         }
